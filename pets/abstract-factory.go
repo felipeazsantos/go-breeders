8d8276d230d73394addf66b59d628268f396bd09
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-09-10 13:55:43 -0300

    Creating a route and handler for the Abstract Factory

--- pets/abstract-factory.go
+++ pets/abstract-factory.go
@@ -6,22 +6,29 @@ import (
 	"github.com/felipeazsantos/breeders/models"
 )
 
+// AnimalInterface is the interface for the types we will return from our abstract factory
+// We'll create two types which implement this interface: DogFromFactory and CatFromFactory.
+// In order to implement this interface, both types must implement the Show() method
 type AnimalInterface interface {
 	Show() string
 }
 
+// DogFromFactory is a type which implements the AnimalInterface, and embeds a dog.
 type DogFromFactory struct {
 	Pet *models.Dog
 }
 
+// Show is a method on the DogFromFactory type, which makes the type implement the AnimalInterface
 func (dff *DogFromFactory) Show() string {
 	return fmt.Sprintf("This animal is a %s", dff.Pet.Breed.Breed)
 }
 
+// CatFromFactory is a type which implements the AnimalInterface, and embeds a cat.
 type CatFromFactory struct {
 	Pet *models.Cat
 }
 
+// Show is a method on the CatFromFactory type, which makes the type implement the AnimalInterface
 func (cff *CatFromFactory) Show() string {
 	return fmt.Sprintf("This animal is a %s", cff.Pet.Breed.Breed)
 }
