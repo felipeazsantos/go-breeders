05de1093736fec136baadc323a1407a06ee88f8f
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-09-22 21:16:18 -0300

    Adding a test for the Cat Breeds handler, using our adapter pattern

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -5,15 +5,33 @@ import (
 	"testing"
 
 	"github.com/felipeazsantos/breeders/configuration"
+	"github.com/felipeazsantos/breeders/models"
 )
 
 var testApp application
 
 func TestMain(m *testing.M) {
+	testBackend := &TestBackend{}
+	testAdapter := &RemoteService{Remote: testBackend}
+
 	// call flag.Parse() here if TestMain uses flags
 	testApp = application{
-		App: configuration.New(nil),
+		App:        configuration.New(nil),
+		catService: testAdapter,
 	}
 
 	os.Exit(m.Run())
+}
+
+type TestBackend struct{}
+
+func (tb *TestBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	breeds := []*models.CatBreed{
+		{
+			ID:      1,
+			Breed:   "Tomcat",
+			Details: "Some details",
+		},
+	}
+	return breeds, nil
 }
